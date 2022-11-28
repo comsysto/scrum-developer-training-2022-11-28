1f05a4473ffd5ec7eb77990b2166b202cbadf6a5
Daniel Trautwein <daniel.trautwein@comsysto.com>
2022-11-28 11:49:00 +0100

    Initial commit

--- src/backend/calculationBackend.js
+++ src/backend/calculationBackend.js
@@ -4,11 +4,40 @@
  * result is simply the mathematical result of the operation
  * palindrome should be true if the result is a palindrome (e.g. 12321) and false otherwise
  */
+
+function checkPalindrome(string) {
+  // find the length of a string
+  const len = string.length;
+
+  // loop through half of the string
+  for (let i = 0; i < len / 2; i++) {
+    // check if first and last string are same
+    if (string[i] !== string[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
 function calculate(operation, a, b) {
-    return {
-        result: 42,
-        palindrome: false
-    };
+  let result;
+  if (operation === "+") {
+    result = a + b;
+  } else if (operation === "-") {
+    result = a - b;
+  } else if (operation === "/") {
+    if (b === 0) {
+      result = undefined;
+    } else {
+      result = a / b;
+    }
+  } else if (operation === "*") {
+    result = a * b;
+  }
+  return {
+    result: result,
+    palindrome: checkPalindrome(result.toString()),
+  };
 }
 
 export default calculate;
--- src/backend/calculationBackend.test.js
+++ src/backend/calculationBackend.test.js
@@ -1,6 +1,43 @@
 import calculate from "./calculationBackend";
 
-test('add 1 and 2 correctly', () => {
-    const result = calculate("+", 1, 2);
-    expect(result.result).toEqual(3);
-})
+test("add 1 and 2 correctly and to not return a palindrome", () => {
+  const result = calculate("+", 10, 2);
+  expect(result.result).toEqual(12);
+  console.log(result.palindrome);
+  expect(result.palindrome).toBe(false);
+});
+
+test("add 12321 and 0 correctly and return a palindrome", () => {
+  const result = calculate("+", 12321, 0);
+  expect(result.result).toEqual(12321);
+  expect(result.palindrome).toBe(true);
+});
+
+// test("divide 12321 through 0 and throw an error", () => {
+//   const result = calculate("/", 12321, 0);
+//   expect(result.result).toEqual(undefined);
+// });
+
+test("substract 12321 and 1 correctly and return not a palindrome", () => {
+  const result = calculate("-", 12321, 1);
+  expect(result.result).toEqual(12320);
+  expect(result.palindrome).toBe(false);
+});
+
+test("multiply 5 and 2 correctly and return not a palindrome", () => {
+  const result = calculate("*", 5, 2);
+  expect(result.result).toEqual(10);
+  expect(result.palindrome).toBe(false);
+});
+
+test("divide 10 and 2 and return a palindrome", () => {
+  const result = calculate("/", 10, 2);
+  expect(result.result).toEqual(5);
+  expect(result.palindrome).toBe(true);
+});
+
+test("add -5 to 0 and return not a palindrome", () => {
+  const result = calculate("+", -5, 0);
+  expect(result.result).toEqual(-5);
+  expect(result.palindrome).toBe(false);
+});
